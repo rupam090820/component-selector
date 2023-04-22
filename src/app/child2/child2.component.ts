af06091265d15796e70170571ee5e81fdaa32200
rupam090820 <rupams441@gmail.com>
2023-04-22 18:51:04 +0530

    first commit

---
+++ src/app/child2/child2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.app-child2',  //here use class component selector
+  templateUrl: './child2.component.html',
+  styleUrls: ['./child2.component.css']
+})
+export class Child2Component {
+
+}
---
+++ src/app/child3/child3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-child3]', // here use attribute component selector
+  templateUrl: './child3.component.html',
+  styleUrls: ['./child3.component.css']
+})
+export class Child3Component {
+ 
+}
